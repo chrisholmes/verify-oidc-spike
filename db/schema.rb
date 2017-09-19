752bd471cf161e988f30d53b1c6518b38f8bbcc1
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2017-09-19 13:54:03 +0100

    add doorkeeper oidc gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919121320) do
+ActiveRecord::Schema.define(version: 20170919123839) do
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer "resource_owner_id", null: false
@@ -50,6 +50,11 @@ ActiveRecord::Schema.define(version: 20170919121320) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
+
+  create_table "oauth_openid_requests", force: :cascade do |t|
+    t.integer "access_grant_id", null: false
+    t.string "nonce", null: false
   end
 
   create_table "users", force: :cascade do |t|
